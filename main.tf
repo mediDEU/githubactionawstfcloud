1fa7154eb45d4ce026cdd6df894dc0c9f51ed990
mediDEU <mehdivasef@gmail.com>
2025-06-10 14:00:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+ required_providers {
+   aws = {
+     source = "hashicorp/aws"
+   }
+ }
+}
+#############################
+
+provider "aws" {
+
+ region = "eu-central-1"
+
+}
+################################
+
 resource "aws_s3_bucket" "example" {
   bucket = "tfcloud-medi-bucket"
  
